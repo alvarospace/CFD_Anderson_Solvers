65998a977aaef1c9cfe07df8535175e512e22b6d
Alvaro Moure <alvaro.moure1998@gmail.com>
2023-05-24 22:49:32 +0200

    Basic step done, iteration is left

--- Nozzle/Subsonic_supersonic_MacCormack/second_attempt.cpp
+++ Nozzle/Subsonic_supersonic_MacCormack/second_attempt.cpp
@@ -3,11 +3,23 @@
 #include <memory>
 #include <functional>
 #include <cmath>
+#include <tuple>
 
 using namespace std;
 
 double const GAMMA = 1.4;
 double const R = 287;
+double const STEPS = 1400;
+
+double forward_FO_scheme(double const *var, int const idx)
+{
+    return var[idx + 1] - var[idx];
+}
+
+double rearward_FO_scheme(double const *var, int const idx)
+{
+    return var[idx] - var[idx - 1];
+}
 
 class StructuredGrid1D
 {
@@ -45,34 +57,88 @@ class Solver
 {
     shared_ptr<StructuredGrid1D> grid;
     double CFL;
-
+    double time;
     vector<double> logA;
-    double *logA;
 
     unique_ptr<Solution> sol;
     double *rho;
     double *v;
     double *T;
+    tuple<int, int> internalRange;
 
-    vector<tuple<Solution, double>> solutions;
+    vector<tuple<unique_ptr<Solution>, double>> solutions;
 
     Solver(shared_ptr<StructuredGrid1D> _grid, double _CFL, unique_ptr<Solution> initConditions, double rho0, double T0)
     {
+        this->time = 0.0;
         this->grid = _grid;
+        this->internalRange = {1, (this->grid->n - 1)};
         this->CFL = CFL;
         this->calculateNozzleArea();
 
-        this->sol = move(initConditions);
+        this->updateSolution(move(initConditions));
+    }
+
+    void run()
+    {
+        // TODO: iterate the process and set an exit condition
+        double dt = this->calculateMinDt();
+        auto tmpSol = make_unique<Solution>(this->grid->n);
+        auto nextSol = make_unique<Solution>(this->grid->n);
+        auto derivatives = make_unique<Solution>(this->grid->n);
+        auto derivatives2 = make_unique<Solution>(this->grid->n);
+
+        // MacCormack predictor-corrector integration (2 steps)
+        this->calculateDerivatives(derivatives.get(), &forward_FO_scheme);
+        this->integrationStep(tmpSol.get(), this->sol.get(), derivatives.get(), dt);
+        this->calculateDerivatives(derivatives2.get(), &rearward_FO_scheme);
+        this->integrationStep(nextSol.get(), this->sol.get(), derivatives.get(), dt);
+        this->time = this->time + dt;
+
+        this->updateSolution(move(nextSol));
+    }
+
+    void calculateAndOverrideDerivativesAverage(Solution *derivatives1, Solution *derivatives2)
+    {
+        for (int i = get<0>(this->internalRange); i < get<1>(this->internalRange); i++)
+        {
+            derivatives1->rho[i] = 0.5 * (derivatives1->rho[i] + derivatives2->rho[i]);
+            derivatives1->v[i] = 0.5 * (derivatives1->v[i] + derivatives2->v[i]);
+            derivatives1->T[i] = 0.5 * (derivatives1->T[i] + derivatives2->T[i]);
+        }
+    }
+
+    void calculateDerivatives(Solution *derivatives, function<double(double const *, int const)> scheme)
+    {
+        for (int i = get<0>(this->internalRange); i < get<1>(this->internalRange); i++)
+        {
+            derivatives->rho[i] = this->deriveDensity(i, scheme);
+            derivatives->v[i] = this->deriveVelocity(i, scheme);
+            derivatives->T[i] = this->deriveTemperature(i, scheme);
+        }
+    }
+
+    void integrationStep(Solution *newSol, Solution *currSol, Solution *derivatives, double dt)
+    {
+        for (int i = get<0>(this->internalRange); i < get<1>(this->internalRange); i++)
+        {
+            newSol->rho[i] = currSol->rho[i] + derivatives->rho[i] * dt;
+            newSol->v[i] = currSol->v[i] + derivatives->v[i] * dt;
+            newSol->T[i] = currSol->T[i] + derivatives->T[i] * dt;
+        }
+    }
+
+    void updateSolution(unique_ptr<Solution> newSol)
+    {
+        this->solutions.push_back({move(this->sol), this->time});
+        this->sol = move(newSol);
         this->rho = this->sol->rho.data();
         this->v = this->sol->v.data();
         this->T = this->sol->T.data();
     }
 
-    void run()
-    {
-        }
-
-    double calculateMinDt()
+    double
+    calculateMinDt()
     {
         auto dts = vector<double>(this->grid->n, 0.0);
 
@@ -105,36 +171,26 @@ class Solver
         }
     }
 
-    double deriveDensity(int idx, function<double(double const *, int const)> squema)
+    double deriveDensity(int idx, function<double(double const *, int const)> scheme)
     {
-        double frac1 = this->rho[idx] * squema(this->v, idx);
-        double frac2 = this->rho[idx] * this->v[idx] * squema(this->logA.data(), idx);
-        double frac3 = this->v[idx] * squema(this->rho, idx);
+        double frac1 = this->rho[idx] * scheme(this->v, idx);
+        double frac2 = this->rho[idx] * this->v[idx] * scheme(this->logA.data(), idx);
+        double frac3 = this->v[idx] * scheme(this->rho, idx);
         return (-1) * (frac1 + frac2 + frac3) / this->grid->dx;
     }
 
-    double deriveVelocity(int idx, function<double(double const *, int const)> squema)
+    double deriveVelocity(int idx, function<double(double const *, int const)> scheme)
     {
-        double frac1 = this->v[idx] * squema(this->v, idx);
-        double frac2 = (1 / GAMMA) * (squema(this->T, idx) + this->T[idx] / this->rho[idx] * squema(this->rho, idx));
+        double frac1 = this->v[idx] * scheme(this->v, idx);
+        double frac2 = (1 / GAMMA) * (scheme(this->T, idx) + this->T[idx] / this->rho[idx] * scheme(this->rho, idx));
         return (-1) * (frac1 + frac2) / this->grid->dx;
     }
 
-    double deriveTemperature(int idx, function<double(double const *, int const)> squema)
+    double deriveTemperature(int idx, function<double(double const *, int const)> scheme)
     {
-        double frac1 = this->v[idx] * squema(this->T, idx);
-        double frac2 = (GAMMA - 1) * this->T[idx] * (squema(this->v, idx) + this->v[idx] * squema(this->logA.data(), idx));
+        double frac1 = this->v[idx] * scheme(this->T, idx);
+        double frac2 = (GAMMA - 1) * this->T[idx] * (scheme(this->v, idx) + this->v[idx] * scheme(this->logA.data(), idx));
         return (-1) * (frac1 + frac2) / this->grid->dx;
-    }
-
-    double forward(double const *var, int const idx)
-    {
-        return var[idx + 1] - var[idx];
-    }
-
-    double rearward(double const *var, int const idx)
-    {
-        return var[idx] - var[idx - 1];
     }
 };
 
